26cc425ebe0133a19ab6e8561dede00333e13641
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-15 15:43:58 -1000

    chirp chirp chirp

--- js/chirp.js
+++ js/chirp.js
@@ -1,6 +1,13 @@
 function chirp(n){
+  var trim;
+  if(n === 0){
+    return ' ';
+  }
+   return 'chirp ' + chirp(--n);
   // FIX ME
 }
+
+chirp(3);
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
