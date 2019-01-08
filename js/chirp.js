569520ef0ae375974737e1d00f8f38650b69d344
Tyler <alexithemia@gmail.com>
2019-01-07 15:03:13 -1000

    Passing

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,12 @@
-function chirp(n){
-  // FIX ME
+function chirp(n) {
+  //basecase
+  if (n === 0) {
+    return '';
+  }
+  //return concatenating strings
+  return chirp(--n) + 'chirp ';
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("#result").html(chirp(3));
 });
