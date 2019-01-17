e4f1a358fcd8b97523d89a2d68c7e0dcc6d0eb00
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-01-16 20:27:54 -1000

    chirp chirp chirp check

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,15 @@
 function chirp(n){
-  // FIX ME
+  if (n === 0) {
+    return " ";
+  }
+ 
+  
+  debugger;
+  return "chirp " + chirp(--n)
 }
+
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
 });
+
